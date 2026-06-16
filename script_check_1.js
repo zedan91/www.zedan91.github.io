273a3656826acd7482493ef85f6022772ffe2d22
zedan0791 <zedan9107@gmail.com>
2026-06-16 19:55:22 +0800

    update

--- script_check_1.js
+++
@@ -1,8 +0,0 @@
-
-(function(){
-  try{
-    if(localStorage.getItem('azobss_admin_role_cache')==='1'){
-      document.documentElement.classList.add('azobss-admin-cache');
-    }
-  }catch(e){}
-})();
