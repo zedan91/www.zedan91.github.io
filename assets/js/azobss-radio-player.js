1dd7d1d79d1125fbf14a2e2a723c8dfcae902449
zedan0791 <zedan9107@gmail.com>
2026-06-25 16:43:44 +0800

    update

--- assets/js/azobss-radio-player.js
+++ assets/js/azobss-radio-player.js
@@ -7,13 +7,68 @@
   window.__AZOBSS_RADIO_PLAYER_LOADED__ = true;
 
   const STATIONS = [
-    { id:'era', name:'ERA', label:'ERA', query:'ERA', country:'MY', web:'https://audio1.syok.my/era' },
-    { id:'sinar', name:'SINAR', label:'SINAR', query:'SINAR', country:'MY', web:'https://audio1.syok.my/sinar' },
-    { id:'zayan', name:'ZAYAN', label:'ZAYAN', query:'ZAYAN', country:'MY', web:'https://audio1.syok.my/zayan' },
-    { id:'ikim', name:'IKIM FM', label:'IKIM FM', query:'IKIM', country:'MY', web:'https://ikimfm.my/' },
-    { id:'custom', name:'Custom URL', label:'Custom URL', query:'', country:'', web:'' }
-  ];
+    // Top / paling popular di Malaysia diletakkan dahulu.
+    { id:'era', name:'ERA', label:'⭐ ERA', query:'ERA', country:'MY', group:'Top Malaysia', web:'https://audio1.syok.my/era' },
+    { id:'hotfm', name:'Hot FM', label:'⭐ Hot FM', query:'Hot FM', country:'MY', group:'Top Malaysia', web:'https://www.hotfm.audio/' },
+    { id:'sinar', name:'SINAR', label:'⭐ SINAR', query:'SINAR', country:'MY', group:'Top Malaysia', web:'https://audio1.syok.my/sinar' },
+    { id:'suria', name:'Suria FM', label:'⭐ Suria FM', query:'Suria FM', country:'MY', group:'Top Malaysia', web:'https://dengar.suria.my/' },
+    { id:'zayan', name:'ZAYAN', label:'⭐ ZAYAN', query:'ZAYAN', country:'MY', group:'Top Malaysia', web:'https://audio1.syok.my/zayan' },
+    { id:'ikim', name:'IKIM FM', label:'⭐ IKIM FM', query:'IKIM FM', country:'MY', group:'Top Malaysia', web:'https://ikimfm.my/' },
+    { id:'radioklasik', name:'Radio Klasik', label:'⭐ Radio Klasik', query:'Radio Klasik', country:'MY', group:'Top Malaysia', web:'https://radio.rtm.gov.my/klasik' },
+    { id:'gegar', name:'GEGAR', label:'⭐ GEGAR', query:'GEGAR', country:'MY', group:'Top Malaysia', web:'https://audio1.syok.my/gegar' },
 
+    { id:'hitz', name:'HITZ', label:'HITZ', query:'HITZ', country:'MY', group:'English / Astro', web:'https://audio1.syok.my/hitz' },
+    { id:'flyfm', name:'Fly FM', label:'Fly FM', query:'Fly FM', country:'MY', group:'English / Media Prima', web:'https://www.flyfm.audio/' },
+    { id:'lite', name:'LITE', label:'LITE', query:'LITE', country:'MY', group:'English / Astro', web:'https://audio1.syok.my/lite' },
+    { id:'mix', name:'MIX', label:'MIX', query:'MIX', country:'MY', group:'English / Astro', web:'https://audio1.syok.my/mix' },
+    { id:'traxx', name:'TraXX FM', label:'TraXX FM', query:'TraXX FM', country:'MY', group:'English / RTM', web:'https://radio.rtm.gov.my/traxx' },
+    { id:'bfm', name:'BFM 89.9', label:'BFM 89.9', query:'BFM 89.9', country:'MY', group:'English / Business', web:'https://www.bfm.my/' },
+
+    { id:'myfm', name:'MY FM', label:'MY FM', query:'MY FM', country:'MY', group:'Chinese', web:'https://audio1.syok.my/my' },
+    { id:'fm988', name:'988 FM', label:'988 FM', query:'988 FM', country:'MY', group:'Chinese', web:'https://www.988.com.my/' },
+    { id:'melody', name:'MELODY', label:'MELODY', query:'MELODY', country:'MY', group:'Chinese', web:'https://audio1.syok.my/melody' },
+    { id:'goxuan', name:'GOXUAN', label:'GOXUAN', query:'GOXUAN', country:'MY', group:'Chinese', web:'https://audio1.syok.my/goxuan' },
+    { id:'aifm', name:'Ai FM', label:'Ai FM', query:'Ai FM', country:'MY', group:'Chinese / RTM', web:'https://radio.rtm.gov.my/ai' },
+    { id:'cityplus', name:'CityPlus FM', label:'CityPlus FM', query:'CityPlus FM', country:'MY', group:'Chinese', web:'https://cityplusfm.my/' },
+
+    { id:'raaga', name:'RAAGA', label:'RAAGA', query:'RAAGA', country:'MY', group:'Tamil / Indian', web:'https://audio1.syok.my/raaga' },
+    { id:'minnalfm', name:'Minnal FM', label:'Minnal FM', query:'Minnal FM', country:'MY', group:'Tamil / Indian', web:'https://radio.rtm.gov.my/minnal' },
+
+    { id:'era_sabah', name:'ERA Sabah', label:'ERA Sabah', query:'ERA Sabah', country:'MY', group:'Sabah / Sarawak', web:'https://audio1.syok.my/erasabah' },
+    { id:'era_sarawak', name:'ERA Sarawak', label:'ERA Sarawak', query:'ERA Sarawak', country:'MY', group:'Sabah / Sarawak', web:'https://audio1.syok.my/erasarawak' },
+    { id:'sabahfm', name:'Sabah FM', label:'Sabah FM', query:'Sabah FM', country:'MY', group:'Sabah / Sarawak', web:'https://radio.rtm.gov.my/sabah' },
+    { id:'sabahvfm', name:'Sabah V FM', label:'Sabah V FM', query:'Sabah V FM', country:'MY', group:'Sabah / Sarawak', web:'https://radio.rtm.gov.my/sabahvfm' },
+    { id:'labuanfm', name:'Labuan FM', label:'Labuan FM', query:'Labuan FM', country:'MY', group:'Sabah / Sarawak', web:'https://radio.rtm.gov.my/labuan' },
+    { id:'sarawakfm', name:'Sarawak FM', label:'Sarawak FM', query:'Sarawak FM', country:'MY', group:'Sabah / Sarawak', web:'https://radio.rtm.gov.my/sarawak' },
+    { id:'waifm', name:'Wai FM', label:'Wai FM', query:'Wai FM', country:'MY', group:'Sabah / Sarawak', web:'https://radio.rtm.gov.my/wai' },
+    { id:'catsfm', name:'Cats FM', label:'Cats FM', query:'Cats FM', country:'MY', group:'Sabah / Sarawak', web:'https://catsfm.my/' },
+    { id:'kupikupifm', name:'Kupi-Kupi FM', label:'Kupi-Kupi FM', query:'Kupi-Kupi FM', country:'MY', group:'Sabah / Sarawak', web:'https://kupikupifm.my/' },
+
+    { id:'nasionalfm', name:'Nasional FM', label:'Nasional FM', query:'Nasional FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/nasional' },
+    { id:'selangorfm', name:'Selangor FM', label:'Selangor FM', query:'Selangor FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/selangor' },
+    { id:'klfm', name:'KL FM', label:'KL FM', query:'KL FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/klfm' },
+    { id:'johorfm', name:'Johor FM', label:'Johor FM', query:'Johor FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/johor' },
+    { id:'kedahfm', name:'Kedah FM', label:'Kedah FM', query:'Kedah FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/kedah' },
+    { id:'kelantanfm', name:'Kelantan FM', label:'Kelantan FM', query:'Kelantan FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/kelantan' },
+    { id:'melakafm', name:'Melaka FM', label:'Melaka FM', query:'Melaka FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/melaka' },
+    { id:'negerifm', name:'Negeri FM', label:'Negeri FM', query:'Negeri FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/negeri' },
+    { id:'pahangfm', name:'Pahang FM', label:'Pahang FM', query:'Pahang FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/pahang' },
+    { id:'perakfm', name:'Perak FM', label:'Perak FM', query:'Perak FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/perak' },
+    { id:'perlisfm', name:'Perlis FM', label:'Perlis FM', query:'Perlis FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/perlis' },
+    { id:'mutiarafm', name:'Mutiara FM', label:'Mutiara FM', query:'Mutiara FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/mutiara' },
+    { id:'terengganufm', name:'Terengganu FM', label:'Terengganu FM', query:'Terengganu FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/terengganu' },
+    { id:'asyikfm', name:'Asyik FM', label:'Asyik FM', query:'Asyik FM', country:'MY', group:'RTM Nasional / Negeri', web:'https://radio.rtm.gov.my/asyik' },
+
+    { id:'bernama', name:'Bernama Radio', label:'Bernama Radio', query:'Bernama Radio', country:'MY', group:'News / Talk / Local', web:'https://bernama.com/radio/' },
+    { id:'buletinfm', name:'Buletin FM', label:'Buletin FM', query:'Buletin FM', country:'MY', group:'News / Talk / Local', web:'https://www.buletinfm.audio/' },
+    { id:'molekfm', name:'Molek FM', label:'Molek FM', query:'Molek FM', country:'MY', group:'News / Talk / Local', web:'https://www.molekfm.audio/' },
+    { id:'manisfm', name:'Manis FM', label:'Manis FM', query:'Manis FM', country:'MY', group:'News / Talk / Local', web:'https://www.manis.fm/' },
+    { id:'bestfm', name:'Best FM', label:'Best FM', query:'Best FM', country:'MY', group:'News / Talk / Local', web:'https://bestfm.com.my/' },
+    { id:'kool101', name:'Kool 101', label:'Kool 101', query:'Kool 101', country:'MY', group:'News / Talk / Local', web:'https://www.kool101.audio/' },
+    { id:'eightfm', name:'Eight FM', label:'Eight FM', query:'Eight FM', country:'MY', group:'News / Talk / Local', web:'https://www.eight.audio/' },
+
+    { id:'custom', name:'Custom URL', label:'Custom URL', query:'', country:'', group:'Custom', web:'' }
+  ];
   const API_BASES = [
     'https://de1.api.radio-browser.info/json/stations/search',
     'https://nl1.api.radio-browser.info/json/stations/search',
